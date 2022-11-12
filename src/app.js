7e84d1d08fa1f47f9994bb651edf69baef6753e1
cromanalcaide <cromanalcaide@gmail.com>
2022-11-12 11:58:32 +0000

    final?

--- src/app.js
+++ src/app.js
@@ -2,10 +2,48 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let intro = ["the-", "our-", "yet-another-", "my-"];
+  let epicEpiteto = [
+    "amazing-",
+    "user-frieldy-",
+    "incredible-",
+    "brand-new-",
+    "uncanny-",
+    "all-different-all-new-",
+    "impossible-",
+    "superior-",
+    "good-old-"
+  ];
+  let epicOutro = [
+    "website-domain",
+    "christmas-present",
+    "extinction-event",
+    "impostor-syndrome"
+  ];
+  let epicDomain = [".com", ".net", ".org", ".ws", ".agency"];
+
+  let newDomain = document.querySelector("#ejemplo2");
+
+  function generateDomain() {
+    for (let i = 0; i < intro.length; i++) {
+      for (let i2 = 0; i2 < epicEpiteto.length; i2++) {
+        for (let i3 = 0; i3 < epicOutro.length; i3++) {
+          for (let i4 = 0; i4 < epicDomain.length; i4++) {
+            let fraseFinal =
+              intro[i] + epicEpiteto[i2] + epicOutro[i3] + epicDomain[i4];
+            let myList = document.createElement("li");
+            myList.innerHTML = fraseFinal;
+            newDomain.appendChild(myList);
+          }
+        }
+      }
+    }
+  }
+
+  // document.getElementById("domainsHere").innerHTML = newDomain;
+  // document.write(newDomain);
+  generateDomain();
+  // const newContent = document.createTextNode(newDomain);
 };
